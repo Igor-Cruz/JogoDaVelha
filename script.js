fbe86ed4bfb7ea65ff598534360d2fb629b31232
Igor-Cruz <igorcruz.it@outlook.com>
2021-07-23 17:40:50 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.querySelectorAll('.item').forEach(item => {
 
 function itemClick(event) {
   let item = event.target.getAttribute('data-item')
-  if (square[item] === '') {
+  if (playing && square[item] === '') {
     square[item] = player
     renderSquare()
     togglePlayer()
@@ -35,7 +35,7 @@ function itemClick(event) {
 function reset() {
   warning = ''
 
-  let random = Math.floor(Math.random() * 2)
+  let random = Math.floor(Math.random() * 2) //
   player = random === 0 ? 'x' : 'o'
   /*if (random === 0) {
     player = 'x'
@@ -57,11 +57,6 @@ function renderSquare() {
   for (let i in square) {
     let item = document.querySelector(`div[data-item=${i}]`)
     item.innerHTML = square[i]
-    /*if (square[i !== '']) {
-      item.innerHTML = square[i]
-    } else {
-      item.innerHTML = ''
-    }*/
   }
 
   checkGame()
@@ -75,9 +70,50 @@ function renderInfo() {
 function togglePlayer() {
   player = player === 'x' ? 'o' : 'x'
   renderInfo()
-  /* if (player === 'x') {
-    player = 'o'
-  } else {
-    player = 'x'
-  }*/
+}
+
+function checkGame() {
+  if (checkWinnerfor('x')) {
+    warning = 'O "x" venceu'
+    playing = false
+  } else if (checkWinnerfor('o')) {
+    warning = 'O "o" venceu'
+    playing = false
+  } else if (isFull()) {
+    warning = 'Deu empate'
+    playing = false
+  }
+}
+
+function checkWinnerfor(player) {
+  let pos = [
+    'a1,a2,a3',
+    'b1,b2,b3',
+    'c1,c2,c3',
+
+    'a1,b1,c1',
+    'a2,b2,c2',
+    'a3,b2,c3',
+
+    'a1,b2,c3',
+    'a3,b2,c1'
+  ]
+
+  for (let w in pos) {
+    let pArray = pos[w].split(',')
+    let hasWon = pArray.every(option => square[option] === player)
+    if (hasWon) {
+      return true
+    }
+  }
+  return false
+}
+
+function isFull() {
+  for (let i in square) {
+    if (square[i] === '') {
+      return false
+    }
+  }
+  return true
 }
